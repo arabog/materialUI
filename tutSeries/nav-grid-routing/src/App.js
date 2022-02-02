73523c26740d9c486598642a5a1db88053d03835
arabambi <arababs2015@gmail.com>
2022-02-03 00:19:14 +0100

    nav card route

---
+++ tutSeries/nav-grid-routing/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+import Navbar from './components/Navbar';
+
+// https://reactrouter.com/docs/en/v6/getting-started/tutorial
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
+
+/*
+MuiButtonBase-root 
+MuiFab-root 
+MuiFab-extended 
+MuiFab-sizeSmall 
+MuiFab-secondary 
+*/
---
+++ tutSeries/nav-grid-routing/src/components/pages/Machine.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MachineLearning = () => {
+  return <div style={{ marginLeft: '500px' }}>Machine Learning</div>;
+};
+
+export default MachineLearning;
